3640b3ae786f5c34c8da8c136eeaa27bdf038a8b
abbaszadeh <hamed.abbaszadeh76@gmail.com>
2023-09-23 17:06:09 +0330

    initial commit

---
+++ src/main/java/ir/negah/bank/CustomerServiceApplication.java
@@ -0,0 +1,17 @@
+package ir.negah.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CREATED_BY abbaszadeh
+ * DATE: ۲۰۲۳/۹/۲۳
+ * TIME: ۱۶:۵۹
+ */
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class);
+    }
+}
